7199ac675931a3aa99a72843346c8f70f682b9df
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-05 11:57:16 -0800

    add redirection on new vendor form submit back to splash page

--- VendorAndOrderTracker/Controllers/VendorController.cs
+++ VendorAndOrderTracker/Controllers/VendorController.cs
@@ -23,7 +23,7 @@ namespace VendorAndOrderTracker.Controllers
     public ActionResult Create(string vendorName, string vendorDescription)
     {
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
-      return RedirectToAction("Index");
+      return RedirectToAction("Index", "Home");
     }
 
     [HttpGet("/vendors/{id}")]
