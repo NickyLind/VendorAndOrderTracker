5c11d1883ac9506c4bd77c69ceddabc4debaaf11
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-05 09:46:46 -0800

    pass vendor tests 4,5,6

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -47,5 +47,49 @@ namespace VendorAndOrderTracker.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "test1";
+      string description01 = "test1";
+      Vendor newVendor1 = new Vendor(name01, description01);
+      string name02 = "test2";
+      string description02 = "test2";
+      Vendor newVendor2 = new Vendor(name02, description02);
+      List<Vendor> newList = new List<Vendor> {newVendor1, newVendor2};
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name01 = "test1";
+      string description01 = "test1";
+      Vendor newVendor1 = new Vendor(name01, description01);
+      string name02 = "test2";
+      string description02 = "test2";
+      Vendor newVendor2 = new Vendor(name02, description02);
+      Vendor result = Vendor.Find(2);
+      Assert.AreEqual(newVendor2, result);
+    }
+
+    [TestMethod]
+    public void AddOrder_PlacesOrderInVendorObject_OrderList()
+    {
+      string name = "test Order";
+      string description = "test Order";
+      int price = 15;
+      string date = "test Order";
+      Order newOrder = new Order(name, description, price, date);
+      List<Order> newList = new List<Order> { newOrder };
+      string vendorName = "test1";
+      string vendorDescription = "test1";
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      newVendor.AddOrder(newOrder);
+      List<Order> result = newVendor.Orders;
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
