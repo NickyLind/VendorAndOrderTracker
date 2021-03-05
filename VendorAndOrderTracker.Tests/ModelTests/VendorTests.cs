2119810e0e88244f62de16dcdd6c4494b06d52aa
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-05 09:29:31 -0800

    pass vendor tests 2,3

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -19,5 +19,23 @@ namespace VendorAndOrderTracker.Tests
       Vendor newVendor = new Vendor ("testname", "testdescription");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsVendorName_String()
+    {
+      string name = "testName";
+      Vendor newVendor = new Vendor(name, "testDescription");
+      string result = newVendor.Name;
+      Assert.AreEqual(result, name);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsVendorDescription_String()
+    {
+      string description = "testDescription";
+      Vendor newVendor = new Vendor("testName", description);
+      string result = newVendor.Description;
+      Assert.AreEqual(result, description);
+    }
   }
 }
