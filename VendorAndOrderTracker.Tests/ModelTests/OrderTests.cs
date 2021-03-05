731018c76f0fbe43e4e5aad7ec8d1ee817c0dee2
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-05 09:06:50 -0800

    pass order tests 3,4,5

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,13 +6,17 @@ using System;
 namespace VendorAndOrderTracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
-    // Test methods go here
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("pancakes");
+      Order newOrder = new Order("pancakes", "pancakes", 15, "march 15");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -20,9 +24,36 @@ namespace VendorAndOrderTracker.Tests
     public void GetDescription_ReturnsDescriptionOfOrder_String()
     {
       string description = "pineapple";
-      Order newOrder = new Order(description);
+      Order newOrder = new Order("pancakes", description, 15, "march 15");
       string result = newOrder.Description;
       Assert.AreEqual(description, result);
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitleOfOrder_String()
+    {
+      string title = "wedding order";
+      Order newOrder = new Order(title, "cake", 15, "march 15");
+      string result = newOrder.Title;
+      Assert.AreEqual(title, result);
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPriceOfOrder_Int()
+    {
+      int price = 25;
+      Order newOrder = new Order("title", "description", price, "date");
+      int result = newOrder.Price;
+      Assert.AreEqual(price, result);
+    }
+
+    [TestMethod]
+    public void GetDate_ReturnsDateOfOrder_String()
+    {
+      string date = "march 25";
+      Order newOrder = new Order("title", "description", 15, date);
+      string result = newOrder.Date;
+      Assert.AreEqual(date, result);
+    }   
   }
 }
