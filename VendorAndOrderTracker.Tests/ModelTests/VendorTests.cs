b5351bcdd5704f3da892537f79fc448c3bee261a
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-05 09:25:44 -0800

    setup basic vendor model and pass first vendor test

---
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorAndOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorAndOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor ("testname", "testdescription");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
